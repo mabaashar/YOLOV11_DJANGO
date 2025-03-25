c9397f51edcee2c334c2588684fbd199c71831f4
mabaashar <mabaashar@gmail.com>
2025-03-25 23:11:40 +0300

    Delete threshold directory

--- threshold/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from threshold.models import Image_upload,Model_info,Video_upload,threshold_component,yolo_model
-# Register your models here.
-
-admin.site.register(Image_upload)
-admin.site.register(Model_info)
-admin.site.register(threshold_component)
-admin.site.register(yolo_model)
